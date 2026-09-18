2755cc525ada74ad76952bfa2e471230f0220012
budius <ronaldopace@gmail.com>
2026-09-18 14:43:11 +0000

    Change to file separator in FileHandler

--- kebab-krafter/src/test/kotlin/com/diconium/mobile/tools/kebabkrafter/v2/parser/json/FileHandlerTest.kt
+++ kebab-krafter/src/test/kotlin/com/diconium/mobile/tools/kebabkrafter/v2/parser/json/FileHandlerTest.kt
@@ -38,43 +38,43 @@ class FileHandlerTest {
 
     @Test
     fun `should find relative parent`() {
-        assertEquals("./", FileHandler(root, file).relativeParent)
+        assertEquals(".$SEP", FileHandler(root, file).relativeParent)
 
-        val child1 = touch("child/someJson.json")
-        assertEquals("./child/", FileHandler(root, child1).relativeParent)
+        val child1 = touch("child${SEP}someJson.json")
+        assertEquals(".${SEP}child$SEP", FileHandler(root, child1).relativeParent)
 
-        val child2 = touch("child0/child1/someJson.json")
-        assertEquals("./child0/child1/", FileHandler(root, child2).relativeParent)
+        val child2 = touch("child0${SEP}child1${SEP}someJson.json")
+        assertEquals(".${SEP}child0${SEP}child1$SEP", FileHandler(root, child2).relativeParent)
     }
 
     @Test
     fun `should find relative file`() {
-        assertEquals("./someJson.json", FileHandler(root, file).relativeFile)
+        assertEquals(".${SEP}someJson.json", FileHandler(root, file).relativeFile)
 
-        val child1 = touch("child/someJson.json")
-        assertEquals("./child/someJson.json", FileHandler(root, child1).relativeFile)
+        val child1 = touch("child${SEP}someJson.json")
+        assertEquals(".${SEP}child${SEP}someJson.json", FileHandler(root, child1).relativeFile)
 
-        val child2 = touch("child0/child1/someJson.json")
-        assertEquals("./child0/child1/someJson.json", FileHandler(root, child2).relativeFile)
+        val child2 = touch("child0${SEP}child1${SEP}someJson.json")
+        assertEquals(".${SEP}child0${SEP}child1${SEP}someJson.json", FileHandler(root, child2).relativeFile)
     }
 
     @Test
     fun `should find file name`() {
         assertEquals("someJson", FileHandler(root, file).name)
 
-        val child1 = touch("child/someJson.json")
+        val child1 = touch("child${SEP}someJson.json")
         assertEquals("someJson", FileHandler(root, child1).name)
 
-        val child2 = touch("child0/child1/someJson.json")
+        val child2 = touch("child0${SEP}child1${SEP}someJson.json")
         assertEquals("someJson", FileHandler(root, child2).name)
     }
 
     @Test
     fun `should compare based on file paths`() {
-        val f1 = touch("child/someJson.json")
+        val f1 = touch("child${SEP}someJson.json")
         val h1 = FileHandler(root, f1)
 
-        val f2 = touch("child/someJson.json")
+        val f2 = touch("child${SEP}someJson.json")
         val h2 = FileHandler(root, f2)
 
         assertEquals(h1, h2)
@@ -91,32 +91,37 @@ class FileHandlerTest {
 
         // both on root
         case("file1.json", "file2.json", "file2.json")
-        case("file1.json", "./file2.json", "file2.json")
+        case("file1.json", ".${SEP}file2.json", "file2.json")
 
         // both on child
-        case("./child/file1.json", "file2.json", "./child/file2.json")
-        case("./child/file1.json", "./file2.json", "./child/file2.json")
+        case(".${SEP}child${SEP}file1.json", "file2.json", ".${SEP}child${SEP}file2.json")
+        case(".${SEP}child${SEP}file1.json", ".${SEP}file2.json", ".${SEP}child${SEP}file2.json")
 
         // source on root, other on child
-        case("file1.json", "child/file2.json", "./child/file2.json")
-        case("file1.json", "./child/file2.json", "./child/file2.json")
+        case("file1.json", "child${SEP}file2.json", ".${SEP}child${SEP}file2.json")
+        case("file1.json", ".${SEP}child${SEP}file2.json", ".${SEP}child${SEP}file2.json")
 
         // other on a sibling
-        case("./child1/file1.json", "../child2/file2.json", "./child2/file2.json")
+        case(".${SEP}child1${SEP}file1.json", "..${SEP}child2${SEP}file2.json", ".${SEP}child2${SEP}file2.json")
 
         // source on child, other on sub-child
-        case("./child/file1.json", "sub-child/file2.json", "./child/sub-child/file2.json")
-        case("./child/file1.json", "./sub-child/file2.json", "./child/sub-child/file2.json")
+        case(".${SEP}child${SEP}file1.json", "sub-child${SEP}file2.json", ".${SEP}child${SEP}sub-child${SEP}file2.json")
+        case(
+            ".${SEP}child${SEP}file1.json",
+            ".${SEP}sub-child${SEP}file2.json",
+            ".${SEP}child${SEP}sub-child${SEP}file2.json",
+        )
     }
 
     @Test
     fun `should return file handler for same root`() {
-        val handler = FileHandler(root, touch("child1/sub/file.json"))
-        val file2 = touch("./child2/file.json")
+        val handler = FileHandler(root, touch("child1${SEP}sub${SEP}file.json"))
+        val file2 = touch(".${SEP}child2${SEP}file.json")
         val expected = FileHandler(root, file2)
-        val output = handler.handlerFromRoot("child2/file.json")
+        val output = handler.handlerFromRoot("child2${SEP}file.json")
         assertEquals(expected, output)
     }
 }
 
 private const val SOME_JSON = """{ "helo":"world" }"""
+private val SEP = File.separator
